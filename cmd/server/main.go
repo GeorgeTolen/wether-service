a48c000ee179414c071fa89aff49146066e48eb1
GeorgeTolen <rediska1203@gmail.com>
2026-06-16 00:58:31 +0500

    add base cron

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,76 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-co-op/gocron/v2"
 )
+
 const httpPort = ":3000"
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		_, err := w.Write([]byte("welcome"))
+		if err != nil {
+			log.Println(err)
+		}
+
 	})
-	http.ListenAndServe(httpPort, r)
+
+	s, err := gocron.NewScheduler()
+	if err != nil {
+		panic(err)
+	}
+	jobs, err := initJobs(s)
+	if err != nil {
+		panic(err)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		err := http.ListenAndServe(httpPort, r)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		fmt.Printf("Strting job: %v\n", jobs[0].ID())
+		s.Start()
+
+	}()
+	wg.Wait()
 }
+
+func initJobs(scheduler gocron.Scheduler) ([]gocron.Job, error) {
+	// create a scheduler
+
+	// add a job to the scheduler
+	j, err := scheduler.NewJob(
+		gocron.DurationJob(
+			10*time.Second,
+		),
+		gocron.NewTask(
+			func() {
+				fmt.Println("Hello cron")
+			},
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return []gocron.Job{j}, err
+}
+
+
